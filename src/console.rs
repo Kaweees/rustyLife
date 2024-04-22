40a8288c56d264b1d3219f637d3704f7dc78d79e
Miguel Villa Floran <miguel.villafloran@gmail.com>
2024-04-22 16:02:43 -0700

    Fix graphics

--- src/console.rs
+++ src/console.rs
@@ -1,7 +1,7 @@
 pub const EXIT_SUCCESS: i32 = 0;
 // pub const EXIT_FAILURE: i32 = -1;
 
-pub const CELL_SIZE: usize = 15;
+pub const CELL_SIZE: usize = 60;
 pub const GRID_WIDTH: usize = 30;
 pub const GRID_HEIGHT: usize = 30;
 
--- src/video/graphics.rs
+++ src/video/graphics.rs
@@ -19,25 +19,37 @@ pub async fn graphics(grid: &mut ToroidalGrid) -> bool {
 fn draw_screen(grid: &mut ToroidalGrid) {
     let w = screen_width();
     let h = screen_height();
-    println!("Expected: ({}, {})", grid.width * grid.cell_size, grid.height * grid.cell_size);
-    println!("Reality: ({}, {})", w, h);
-
-    // /* Draw cells */
-    // for i in 0..(grid.width) {
-    //   for j in 0..(grid.height) {
-    //     draw_rectangle((i * grid.cell_size) as f32 / w, (j * grid.cell_size) as f32 / h, (grid.cell_size) as f32, (grid.cell_size) as f32,  if grid.get_toroidal_grid(i as isize, j as isize) == CellOptions::ALIVE {GREEN} else {BLACK});
-    //   }
-    // }
-
+    // println!("Expected: ({}, {})", grid.width * grid.cell_size, grid.height * grid.cell_size);
+    // println!("Reality: ({}, {})", w, h);
+    // println!("{}", (i * grid.cell_size) as f32 / h);
+    
+    /* Draw cells */
+    let mut align_x: f32 = 0.0;
+    let mut align_y: f32 = 0.0;
+    for i in 0..(grid.width) {
+      for j in 0..(grid.height) {
+        draw_rectangle(align_x, align_y, w / (grid.width as f32), h / (grid.height as f32),  if grid.get_toroidal_grid(i as isize, j as isize) == CellOptions::ALIVE {GREEN} else {BLACK});
+        align_y += h / (grid.height as f32);
+      }
+      align_x += w / (grid.width as f32);
+      align_y = 0.0;
+    }
+    
+    
     /* Draw horizontal lines */
-    for i in 1..(grid.height) {
-      draw_line(0 as f32 / w, (i * grid.cell_size) as f32, w, (i * grid.cell_size) as f32 / h, 1.0, WHITE)
+    align_y = 0.0;
+    for _i in 0..(grid.height) {
+      // println!("{}", align);
+      // println!("{}", h / (grid.width as f32));
+      // println!("{}", (i * grid.cell_size) as f32);
+      draw_line(0 as f32, align_y, w, align_y, 1.0, WHITE);
+      align_y += h / (grid.height as f32);
     }
 
     /* Draw vertical lines */
-    for j in 1..(grid.width) {
-      draw_line((j * grid.cell_size) as f32, 0.0, (j * grid.cell_size) as f32, (grid.height * grid.cell_size) as f32, 1.0, WHITE)
+    align_x = 0.0;
+    for _j in 0..(grid.width) {
+      draw_line(align_x, 0.0, align_x, h, 1.0, WHITE);
+      align_x += w / (grid.width as f32);
     }
-    
-    draw_text("Conway's Game of Life", 1.0, w / 20.0, w / 20.0, WHITE);
 }
